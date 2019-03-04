32f47874aa80bed00adc33f10c78adf64c01e9bd
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-03-04 05:59:35 -0800

    Add files via upload

---
+++ WaterSongFixed.java
@@ -0,0 +1,24 @@
+public class WaterSongFixed{
+ public static void main(string[] args){
+    int waterNum = 9; 
+      string word = "bottle";
+     while(waterNum>0)
+   {
+     
+      {
+          word="bottles";
+        }
+         System.out.println(waterNum + " " + word + "of water on the wall");
+         System.out.println(WaterNum + " " + word + "of water");
+         System.out.println("Take one down.");
+         System.out.println("Pass it around");
+         waterNum=waterNum-1;
+           if(waterNum==1)
+          if(waterNum>0)
+    {
+      System.out.println(waterNum + " " + word + "of water on the wall");
+     }
+    }
+   }
+}
+
