8d70720ae2964cbb069d4c4a0b53eba3068ebb77
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-03-10 01:29:15 -0800

    class test.java

---
+++ class test.java
@@ -0,0 +1,14 @@
+Class Test {
+   public static void main(String [] args}  {
+      int x = 0;
+      int y = 0;
+      while ( x<5) (
+      
+
+       y = y+ x;
+      
+      system.out.print (x + "" + y +" ");
+      x x+1;
+     }
+   }
+ }
