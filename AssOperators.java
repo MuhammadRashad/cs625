c08175a10a1ea3e2bb081cea22a93f260fbcc802
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-03-14 20:03:18 -0700

    assOperators.java

---
+++ AssOperators.java
@@ -0,0 +1,30 @@
+public class AssOperators
+{
+    public static void main(String[] args)
+      {
+            int a = 20, b = 10, c,d,e= 10,f= 4, g = 9;
+
+             // simple assignment operator
+              c = b;
+              System.out.println("value of c = " +c);
+
+               a = a+1;
+               b = b-1;
+               e = e*2;
+               f = f*2;
+               System.out.println("a,b,e,f = " +a + "," + b + "," + e + "," + f);
+              
+               a = a-1;
+               b = b+1;
+               e = e/2;
+               f = f*2;
+              
+             // shorthand assignment operator
+              
+             a += 1;
+             b -= 1;
+             e *= 2;
+             f /= 2;
+             System.out.println("a,b,e,f (using shorthand operators)= " + a +"," + b +"," + e +"," + f);
+     }
+}  
