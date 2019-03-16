c5b6ba763ad5fddeb6d7fe52237f9212426b9317
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-03-16 08:25:26 -0700

    WhileDemo.java

---
+++ WhileDemo.java
@@ -0,0 +1,11 @@
+class Whiledemo {
+      public static void main(String args[]) {
+           char ch;
+
+          ch = 'a';
+          while(ch <= 'z') {
+             System.out.print(ch);
+             ch++;
+         }
+     }
+ }
