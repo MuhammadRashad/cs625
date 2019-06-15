43e02f01132b480f1ef9629302e032380c9e248a
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-06-15 06:25:58 -0700

    Add files via upload

---
+++ ACopy.java
@@ -0,0 +1,18 @@
+class ACopy {
+   public static void main(String args[]) {
+      int i;
+      int nums1[] = new int[10];
+      int nums2[] = new int[10];
+
+
+      for(i=0; i < nums1.length; i++)
+            nums[i] = i;
+
+        if(nums2.length >= nums1.length)
+           for(i = 0; i < nums1.length; i++)
+                nums2[i] = nums[i];
+
+      for(i=0; i < nums2.length; i++)
+          System.out.println(nums2[i] + " ");
+  }
+}
