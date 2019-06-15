4a446cbd18058b87acce6dcd0cb79b96eda00e25
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-06-15 06:32:48 -0700

    Add files via upload

---
+++ Moon.java
@@ -0,0 +1,13 @@
+class Moon {
+    public static void main (String args []) {
+      double earthweight ;
+      double moonweight ;
+
+      earthweight = 165 ;
+     
+      moonweight = earthweight * 0.17;
+  
+      System.out.println(earthweight + " earth-pounds is equilant to " + moonweight + " moon-pounds. ");
+   
+    }
+  }
