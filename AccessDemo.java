88619950b56af4b23bccde33413654d84b523acf
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-06-15 06:25:24 -0700

    Add files via upload

---
+++ AccessDemo.java
@@ -0,0 +1,12 @@
+class AccessDemo {
+     public static void main(String args[]) {
+        myclass ob = new myclass();
+
+        ob.setAlpha(-99);
+        System.out.println("ob.alpha is " + ob.getAlpha());
+          ob.alpha = 10;
+      
+        ob.beta = 88;
+        ob.gamma = 99;
+   }
+ }
