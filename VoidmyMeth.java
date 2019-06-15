a5928ff1d6fccff52d4f5074f4a0e509f40f76ee
MuhammadRashad <48056256+MuhammadRashad@users.noreply.github.com>
2019-06-15 06:40:44 -0700

    Add files via upload

---
+++ VoidmyMeth.java
@@ -0,0 +1,12 @@
+class VoidmyMeth {
+   public static void main (String args[]) {
+     
+      void myMeth()
+      int i ;
+   
+     for(i=0; i<10; i++) {
+        if(i == 5) return;
+         System.out.println() ;
+    }
+   }
+ }
